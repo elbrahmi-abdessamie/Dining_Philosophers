223659c5f973783c22227b882ff307f1435dd339
elbrahmi-abdessamie <elbrahmi-abdessamie@users.noreply.github.com>
2023-08-23 09:23:46 -0400

    Syncro Listner

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: aelbrahm <aelbrahm@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 17:56:12 by aelbrahm          #+#    #+#             */
-/*   Updated: 2023/08/05 16:27:49 by aelbrahm         ###   ########.fr       */
+/*   Updated: 2023/08/23 09:17:58 by aelbrahm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,16 @@ void	listner(t_share_data *data)
 		if (iter == (int)(data->philo_num - 1))
 			iter = 0;
 		ft_usleep(data->time_to_die + 1);
+		pthread_mutex_lock(&data->l_eat_time);
 		if ((time_line() - data->philo[iter].last_meal) >= data->time_to_die)
 		{
+			pthread_mutex_unlock(&data->l_eat_time);
 			ft_write_stat("died ||\n", &data->philo[iter]);
 			data->sim = 1;
 			data->philo[iter].stat = TS_TERMINATED;
 			return ;
 		}
+		pthread_mutex_unlock(&data->l_eat_time);
 		iter++;
 	}
 	return ;
@@ -38,9 +41,9 @@ int	main(int ac, char **av)
 {
 	t_share_data	data;
 	t_philo			philos[MAX_P];
-	int				iter;
+	// int				iter;
 
-	iter = 0;
+	// iter = 0;
 	if (ac < 5 || ac > 6)
 		return (_err_(W_ARGS), 1);
 	if (!data_init(ac, av, &data))
